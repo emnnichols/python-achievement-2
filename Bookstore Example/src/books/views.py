2436e8d0cc60b1cd838afcfd907bd4b53bec3ea1
Emily Nichols <emnnichols@outlook.com>
2024-09-02 15:08:09 -0700

    Added authentication to Bookstore example

--- Bookstore Example/src/books/views.py
+++ Bookstore Example/src/books/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
 
 # Create your views here.
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
   model = Book
   template_name = 'books/main.html'
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
   model = Book
   template_name = 'books/detail.html'
--- Bookstore Example/src/sales/views.py
+++ Bookstore Example/src/sales/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
   return render(request, 'sales/home.html')
+
+@login_required
+def records(request):
+  return render(request, 'sales/records.html')
